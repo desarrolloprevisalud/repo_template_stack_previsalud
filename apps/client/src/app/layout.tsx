48f80696117daf02c3f838d7bac9587de5fc5482
Desarrollador Bonnadona 2 <159072115+ingdev2@users.noreply.github.com>
2024-10-04 13:17:16 -0500

    Update layout.tsx
    icon ready

--- apps/client/src/app/layout.tsx
+++ apps/client/src/app/layout.tsx
@@ -22,9 +22,6 @@ export const metadata: Metadata = {
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <html lang="en" style={{ margin: 0, padding: 0 }}>
-      <head className="container-head-app">
-        <link rel="icon" href="/favicon.ico" sizes="any" />
-      </head>
 
       <body className={inter.className} style={{ margin: 0, padding: 0 }}>
         <Providers>
